488bd5247c0c12c05c1732f2ba28d673806dd8d7
Yoel Jasner <yjasner@gmail.com>
2018-03-27 22:21:36 +0300

    Controller class build

---
+++ MyNewService/MyNewService/NewFileCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService
+{
+    class NewFileCommand:ICommand
+    {
+        private IImageModal modal;
+
+        public NewFileCommand(IImageModal newModal)
+        {
+            this.modal = newModal;            // Storing the Modal
+        }
+
+        public string Execute(string[] args, out bool result)
+        {
+            return modal.AddFile(args, out result);
+        }
+    }
+}
