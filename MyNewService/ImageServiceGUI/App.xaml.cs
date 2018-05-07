b09a93ed31851c67a66c0d11e64a52d1547a2af2
SZ <seanzaretzky23@gmail.com>
2018-05-07 19:38:59 +0300

    commit after correction

--- MyNewService/ImageServiceGUI/App.xaml.cs
+++ MyNewService/ImageServiceGUI/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace wpfex2
+namespace ImageServiceGUI
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- MyNewService/ImageServiceGUI/Properties/AssemblyInfo.cs
+++ MyNewService/ImageServiceGUI/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("wpfex2")]
+[assembly: AssemblyTitle("ImageServiceGUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("wpfex2")]
+[assembly: AssemblyProduct("ImageServiceGUI")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
